6f5695016352e38a9cc17f274b2d297fea37810d
Nico Heise <nico.heise@googlemai.com>
2011-01-12 11:53:39 +0100

    new branche

---
+++ src/de/wi08e/myhome/nodeplugins/camera/Main.java
@@ -0,0 +1,53 @@
+package de.wi08e.myhome.nodeplugins.camera;
+
+import java.util.Map;
+
+import org.w3c.dom.Node;
+
+import de.wi08e.myhome.model.datagram.Datagram;
+import de.wi08e.myhome.nodeplugins.NodePlugin;
+import de.wi08e.myhome.nodeplugins.NodePluginEvent;
+import de.wi08e.myhome.nodeplugins.NodePluginException;
+
+public class Main implements NodePlugin{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initiate(NodePluginEvent event, Map<String, String> properties,
+			Node data) throws NodePluginException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void chainReceiveDatagram(Datagram datagram) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void chainSendDatagramm(Datagram datagram) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCategory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
