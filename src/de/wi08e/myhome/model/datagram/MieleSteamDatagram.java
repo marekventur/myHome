d83ace54094047ce0c34d7e40074fc63db53c0b0
aleks123 <skworez2@rambler.ru>
2011-01-18 10:16:38 +0100

    update

---
+++ src/de/wi08e/myhome/model/datagram/MieleSteamDatagram.java
@@ -0,0 +1,15 @@
+package de.wi08e.myhome.model.datagram;
+
+import de.wi08e.myhome.model.Node;
+
+
+public class MieleSteamDatagram extends BroadcastDatagram
+{
+
+	public MieleSteamDatagram(Node sender) {
+		super(sender);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
